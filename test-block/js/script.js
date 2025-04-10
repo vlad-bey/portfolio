dfd8527d1d31a31263993696e1ef5c1e1734238d
vlad-bey <vladbei4545@gmail.com>
2025-04-10 18:26:30 +0300

    Add files via upload

---
+++ test-block/js/script.js
@@ -0,0 +1,13 @@
+"use strict";
+
+
+
+let documentActions = (e) => {
+  const targetElement = e.target;
+
+  if (targetElement.closest(".icon-menu")) {
+    document.documentElement.classList.toggle("open-menu");
+  }
+};
+
+document.addEventListener("click", documentActions);
